31babd4c4f9b574f4daeae57c2e456d454bf53ae
Francisco Sanchez <javisan0105@gmail.com>
2021-10-28 10:42:21 -0430

    using git for heroku deployment

--- index.js
+++ index.js
@@ -87,8 +87,8 @@ io.on('connection', (socket) => {
   })
   socket.on('solicitarTasa',async(arg)=>{     //escucho mensajes desde el cliente con el evento 
     //const tasa=await baseDeDatos.find()
-    //const id="6179791356e3bdc0e5897079"
-    const tasa= await baseDeDatos.findById( "617aa7a650862c9923fe4801",(error,data)=>{
+    const id="6179791356e3bdc0e5897079"
+    const tasa= await baseDeDatos.findById( id,(error,data)=>{
       if(error){
         console.log(error)
       }
