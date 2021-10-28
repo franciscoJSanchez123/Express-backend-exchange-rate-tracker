b75bda1933bc4fa559cb80098492ef4ba8e64d99
Francisco Sanchez <javisan0105@gmail.com>
2021-10-28 09:04:57 -0430

    using git for heroku deployment

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ io.on('connection', (socket) => {
   socket.on('solicitarTasa',async(arg)=>{     //escucho mensajes desde el cliente con el evento 
     //const tasa=await baseDeDatos.find()
     const id="6179791356e3bdc0e5897079"
-    const tasa= await baseDeDatos.findById(id,'tasa').exec()
+    const tasa= await baseDeDatos.findOne({ _id:"6179791356e3bdc0e5897079"})
     socket.emit('ultimaTasa',`${tasa}`)
   })
 
