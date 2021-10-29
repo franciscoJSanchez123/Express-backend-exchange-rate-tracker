ff0ea5bc5eff3f757965180e2915ac5ae1c4c4cb
Francisco Sanchez <javisan0105@gmail.com>
2021-10-29 15:52:10 -0430

    using git for heroku deployment

--- index.js
+++ index.js
@@ -65,13 +65,15 @@ cron.schedule('04 09 1-31 1-12 1-5 ',async()=>{
 },{scheduled: true,
   timezone: 'America/Caracas'});
 
-cron.schedule('05 16 1-31 1-12 1-5 ',async()=>{
-  const tasa=await PriceInstagram(false,true)
+cron.schedule('24 16 1-31 1-12 1-5 ',async()=>{
+
+  console.log('todo bien con cron')
+  /*const tasa=await PriceInstagram(false,true)
   const newTasa=await new baseDeDatos(tasa)
   const tasa2=await newTasa.save()
   console.log('todo bien',tasa2)
   console.log(tasa)
-  io.emit('tasa',`${tasa}`)
+  io.emit('tasa',`${tasa}`)*/
 },{scheduled: true,
   timezone: 'America/Caracas'});
 
