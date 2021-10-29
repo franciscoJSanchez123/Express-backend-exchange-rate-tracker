82581b6e539cd19661c3c9fcf16e6cec2aa91cfa
Francisco Sanchez <javisan0105@gmail.com>
2021-10-29 07:22:27 -0430

    using git for heroku deployment

--- index.js
+++ index.js
@@ -55,10 +55,8 @@ const PriceInstagram=require('./service')
 const cron =require('node-cron');
 
 
-cron.schedule('51 07 1-31 1-12 1-5 ',async()=>{
+cron.schedule('53 07 1-31 1-12 1-5 ',async()=>{
   const tasa=await PriceInstagram(false,true)
-  await baseDeDatos.create(tasa)
-  
   console.log(tasa)
   io.emit('tasa',`${tasa}`)
 },{scheduled: true,
