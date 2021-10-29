84d40482f2b6aad4397eb2e28f507ba84c5f2470
Francisco Sanchez <javisan0105@gmail.com>
2021-10-29 10:20:58 -0430

    using git for heroku deployment

--- service.js
+++ service.js
@@ -47,8 +47,9 @@ const PriceInstagram=async (is9AM,is1PM)=>{
     console.log(estracto)
     console.log(tasa2)
     console.log(bolivar)*/
-    const tasa=parseFloat(bolivar.replace(',',"."))
-    const respuesta = {tasa}
+    const tasa=parseFloat(bolivar.replace(',',"."));
+    const fecha=new Date().toLocaleDateString();
+    const respuesta = {tasa,fecha}
     return respuesta
 }
 
